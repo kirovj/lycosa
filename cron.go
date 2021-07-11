075f4c6d8aaf93fc4e54d99e41a58267a8472ddb
J-wyt <546108612@qq.com>
2021-07-12 02:37:59 +0800

    add cron test

--- cron.go
+++ cron.go
@@ -1,5 +1,24 @@
 package lycosa
 
-func Parse(cron string) {
+import (
+	"fmt"
 
+	"github.com/robfig/cron/v3"
+)
+
+func Run() {
+	c := cron.New()
+	_, err := c.AddFunc("*/5 * * * * *", print5)
+	if err != nil {
+		fmt.Println(err)
+	}
+	c.Start()
+	defer c.Stop()
+	select { /* select: to keep this running */
+	}
+}
+
+//执行函数
+func print5() {
+	fmt.Println("每5s执行一次cron")
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module lycosa
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.7.2
+require (
+	github.com/gin-gonic/gin v1.7.2
+	github.com/robfig/cron/v3 v3.0.1
+)
